4bcf54af6cc916ee7d6493c5061b67452421d20b
mhamedy <mohamed01014577@gmail.com>
2022-03-26 19:34:56 +0200

    change color

--- Checkers/checkers/constants.py
+++ Checkers/checkers/constants.py
@@ -5,10 +5,10 @@ ROWS, COLS = 8, 8
 SQUARE_SIZE = WIDTH//COLS
 
 # color
-RED = (255, 0, 0)
-WHITE = (255, 255, 255)
+RED = (255, 255, 255)   #RED   --> white
+WHITE = (0, 255, 0)     #WHITE --> green
 BLACK = (0, 0, 0)
-BLUE = (0, 0, 255)
-GREY = (128,128,128)
+BLUE = (0, 0, 255)      
+GREY = (128,128,128)    
 
 CROWN = pygame.transform.scale(pygame.image.load('assets/crown.png'), (44, 25))
