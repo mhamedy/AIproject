36437810275bd349a48b1a536624496095c1b074
mhamedy <mohamed01014577@gmail.com>
2022-04-21 11:15:08 +0200

    add algo

--- Checkers/main.py
+++ Checkers/main.py
@@ -1,7 +1,8 @@
-# game main
+# Assets: https://techwithtim.net/wp-content/uploads/2020/09/assets.zip
 import pygame
-from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED, WHITE
 from checkers.game import Game
+from minimax.algorithm import minimax
 
 FPS = 60
 
@@ -21,6 +22,10 @@ def main():
 
     while run:
         clock.tick(FPS)
+        
+        if game.turn == WHITE:
+            value, new_board = minimax(game.get_board(), 4, WHITE, game)
+            game.ai_move(new_board)
 
         if game.winner() != None:
             print(game.winner())
